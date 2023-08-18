e85d0fe73b2bdc8a082f781bccf2c228280c6870
sazibkhan <sazibkhancse@gmail.com>
2023-08-18 11:35:19 +0600

    ok

--- src/main/java/com/inventory/inventoryservice/company/model/CompanyEntity.java
+++ src/main/java/com/inventory/inventoryservice/company/model/CompanyEntity.java
@@ -15,6 +15,7 @@ public class CompanyEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(name = "company_name")
     private String companyName;
 
