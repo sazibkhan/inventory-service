019d7accab955c3f9ae45fb3e6826c0c9f19a9a3
sazibkhan <sazibkhancse@gmail.com>
2024-03-26 21:01:56 +0600

    product update and delete

--- src/main/java/com/inventory/inventoryservice/product/ProductController.java
+++ src/main/java/com/inventory/inventoryservice/product/ProductController.java
@@ -3,14 +3,22 @@ package com.inventory.inventoryservice.product;
 import com.inventory.inventoryservice.brand.model.BrandDto;
 import com.inventory.inventoryservice.brand.model.BrandRest;
 import com.inventory.inventoryservice.brand.model.BrandSearchDto;
+import com.inventory.inventoryservice.category.model.CategoryEntity;
+import com.inventory.inventoryservice.category.model.CategoryRest;
 import com.inventory.inventoryservice.product.model.ProductDto;
 import com.inventory.inventoryservice.product.model.ProductEntity;
 import com.inventory.inventoryservice.product.model.ProductRest;
 import com.inventory.inventoryservice.product.model.ProductSearchDto;
+import com.inventory.inventoryservice.purchase.model.PurchaseDto;
+import com.inventory.inventoryservice.purchase.model.PurchaseRest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -19,13 +27,21 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final ProductService productService;
-
-
     @PostMapping
     public ResponseEntity<ProductRest> saveProduct(@RequestBody ProductDto productDto) {
-
             ProductRest productRest = productService.saveProduct(productDto);
             return ResponseEntity.status(HttpStatus.OK).body(productRest);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductRest> updateProduct(@PathVariable Long id,
+                                                     @RequestBody @Valid ProductDto productDto) {
+        ProductRest productRest = productService.updateProduct(id,productDto);
+        return ResponseEntity.status(HttpStatus.OK).body(productRest);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Deleted Successfully.");
     }
 
     @PostMapping("/search-page")
@@ -33,10 +49,8 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK)
             .body(productService.searchPage(searchDto));
     }
-
     @PostMapping("/search-list")
     public ResponseEntity<?> searchList(@RequestBody ProductSearchDto searchDto) {
-
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productService.searchList(searchDto));
     }
--- src/main/java/com/inventory/inventoryservice/product/ProductService.java
+++ src/main/java/com/inventory/inventoryservice/product/ProductService.java
@@ -7,16 +7,20 @@ import com.inventory.inventoryservice.brand.model.BrandEntity;
 import com.inventory.inventoryservice.brand.model.BrandRest;
 import com.inventory.inventoryservice.brand.model.BrandSearchDto;
 import com.inventory.inventoryservice.category.CategoryValidatorService;
+import com.inventory.inventoryservice.category.model.CategoryRest;
 import com.inventory.inventoryservice.product.model.ProductDto;
 import com.inventory.inventoryservice.product.model.ProductEntity;
 import com.inventory.inventoryservice.product.model.ProductRest;
 import com.inventory.inventoryservice.product.model.ProductSearchDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,18 +29,30 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ProductQueryService productQueryService;
     private final ProductValidatorService productValidatorService;
-
+    private final BrandValidatorService brandValidatorService;
+    private final CategoryValidatorService categoryValidatorService;
     public ProductRest saveProduct(ProductDto productDto) {
-
         ProductEntity product = productValidatorService.validateAndReturnProductSave(productDto);
-
         productRepository.save(product);
         return ProductTransform.toProductRest(product);
     }
 
-
-
-
+    public ProductRest updateProduct(Long id,ProductDto productDTO) {
+        var productEntity = productValidatorService.ifFoundByIdReturnElseThrow(id);
+        var brandEntity = brandValidatorService.ifFoundByIdReturnElseThrow(productDTO.getBrandId());
+        var categoryEntity = categoryValidatorService.ifFoundByIdReturnElseThrow(productDTO.getCategoryId());
+            productEntity.setProductName(productDTO.getProductName());
+            productEntity.setPurchasePrice(productDTO.getPurchasePrice());
+            productEntity.setSalesPrice(productDTO.getSalesPrice());
+            productEntity.setBrand(brandEntity);
+            productEntity.setCategory(categoryEntity);
+            productRepository.save(productEntity);
+        return ProductTransform.toProductRest(productEntity);
+    }
+    public void deleteProduct(Long id) {
+        var productEntity = productValidatorService.ifFoundByIdReturnElseThrow(id);
+        productRepository.deleteById(productEntity.getId());
+    }
 
 
 
@@ -45,19 +61,14 @@ public class ProductService {
 
 
   public Page<ProductRest> searchPage(ProductSearchDto searchDto) {
-
       Page<ProductEntity> page = productQueryService.searchPage(searchDto);
-
       List<ProductRest> productRestList = ProductTransform.toProductRestList(page.getContent());
-
       return new PageImpl<>(productRestList, page.getPageable(), page.getTotalElements());
-
   }
-
-
     public List<ProductRest> searchList(ProductSearchDto searchDto) {
         return ProductTransform.toProductRestList(productQueryService.searchList(searchDto));
     }
+
 
 
 }
