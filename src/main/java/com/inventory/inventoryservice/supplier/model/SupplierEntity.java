6e9ede154d8fd9baf2bf37d41d423fbdabb63584
Mahadi Hasan <mahadi.diu791@gmail.com>
2024-03-29 16:27:10 +0600

    updated

--- src/main/java/com/inventory/inventoryservice/supplier/model/SupplierEntity.java
+++ src/main/java/com/inventory/inventoryservice/supplier/model/SupplierEntity.java
@@ -11,33 +11,37 @@ import java.time.LocalDateTime;
 @Table(name = "suppliers")
 public class SupplierEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(name = "supplier_name")
-    private String supplierName;
+  @Column(name = "supplier_name")
+  private String supplierName;
 
-    @Column(name = "supplier_address")
-    private String supplierAddress;
+  @Column(name = "supplier_address")
+  private String supplierAddress;
 
-    @Column(name = "phone_number")
-    private String phoneNumber;
+  @Column(name = "phone_number")
+  private String phoneNumber;
 
-    @Column(name = "enabled")
-    private boolean enabled;
+  @Column(name = "supplier_type")
+  @Enumerated(EnumType.STRING)
+  private SupplierTypeEnum supplierType;
 
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
+  @Column(name = "enabled")
+  private boolean enabled;
 
-    @Column(name = "created_by")
-    private Long createdBy;
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+  @Column(name = "created_by")
+  private Long createdBy;
 
-    @Column(name = "updated_by")
-    private  Long updatedBy;
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @Column(name = "updated_by")
+  private Long updatedBy;
 
 
 }
---
+++ src/main/java/com/inventory/inventoryservice/supplier/model/SupplierTypeEnum.java
@@ -0,0 +1,5 @@
+package com.inventory.inventoryservice.supplier.model;
+
+public enum SupplierTypeEnum {
+  Company, Manufacturer, Person
+}
