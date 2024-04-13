7ebf44432bc9fd42f4b01a88b5038f80392b8bd6
Mahadi Hasan <mahadi.diu791@gmail.com>
2024-04-13 14:52:57 +0600

    updatd

--- src/main/java/com/inventory/inventoryservice/security/model/UserRoleEntity.java
+++ src/main/java/com/inventory/inventoryservice/security/model/UserRoleEntity.java
@@ -1,6 +1,5 @@
 package com.inventory.inventoryservice.security.model;
 
-import com.inventory.inventoryservice.security.model.UserEntity;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,24 +11,24 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @Table(name = "auth_users_role")
-public class UserRoleEntity {
+public class UserRoleEntity{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne
-    @JoinColumn(
-            name = "user_id",
-            nullable = false,
-            foreignKey = @ForeignKey(
-                    name = "auth_users_role_user_id_fk"
-            )
+  @ManyToOne
+  @JoinColumn(
+    name = "user_id",
+    nullable = false,
+    foreignKey = @ForeignKey(
+      name = "auth_users_role_user_id_fk"
     )
-    private UserEntity user;
+  )
+  private UserEntity user;
 
-    @NotBlank
-    @Size(min = 6, max = 20, message = "Role name must be between 6 to 20")
-    @Column(name = "role_name")
-    private String roleName;
+  @NotBlank
+  @Size(min = 6, max = 20, message = "Role name must be between 6 to 20")
+  @Column(name = "role_name")
+  private String roleName;
 }
